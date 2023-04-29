2cb8616f34f33090690c12bf19f8d5842236df29
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-04-29 21:42:05 +0200

    create test: when string is empty

--- src/main/java/com/codurance/katalyst/StringCalculator.java
+++ src/main/java/com/codurance/katalyst/StringCalculator.java
@@ -1,5 +1,9 @@
 package com.codurance.katalyst;
 
 public class StringCalculator {
+
+    public Integer add(String string) {
+        return null;
+    }
     
 }
--- src/test/java/com/codurance/katalyst/StringCalculatorTest.java
+++ src/test/java/com/codurance/katalyst/StringCalculatorTest.java
@@ -1,5 +1,14 @@
 package com.codurance.katalyst;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class StringCalculatorTest {
     
+    @Test
+    public void add_when_string_is_empty(){
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(0, calculator.add(""));
+    } 
 }
