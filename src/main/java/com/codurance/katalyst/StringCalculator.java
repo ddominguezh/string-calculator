ef96103df2324f85e0511925232bae3f030fb7b5
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-04-29 21:58:05 +0200

    fix test: when string contains many numbers

--- src/main/java/com/codurance/katalyst/StringCalculator.java
+++ src/main/java/com/codurance/katalyst/StringCalculator.java
@@ -8,12 +8,9 @@ public class StringCalculator {
         if("".equals(numbers)){
             return 0;
         }
-
-        String[] values = numbers.split(",", -1);
-        if(values.length == 1){
-            return Integer.valueOf(values[0]);
-        }
-        return Integer.valueOf(values[0]) + Integer.valueOf(values[1]);
+        return Arrays.stream(numbers.split(",", -1))
+            .map(number -> Integer.valueOf(number))
+            .reduce(0, (a, b) -> a + b);
     }
     
 }
