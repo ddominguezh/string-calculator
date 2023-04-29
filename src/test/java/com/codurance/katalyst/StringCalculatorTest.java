a7da75e666d624c108f4ff4874e5818c91ab3a9d
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-04-29 21:50:22 +0200

    create test: when string contains two numbers

--- src/test/java/com/codurance/katalyst/StringCalculatorTest.java
+++ src/test/java/com/codurance/katalyst/StringCalculatorTest.java
@@ -16,5 +16,11 @@ public class StringCalculatorTest {
     public void add_when_string_contains_only_one_number(){
         StringCalculator calculator = new StringCalculator();
         assertEquals(5, calculator.add("5"));
-    } 
+    }
+
+    @Test
+    public void add_when_string_contains_two_numbers(){
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(53, calculator.add("4,49"));
+    }
 }
