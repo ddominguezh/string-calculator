baf7db1b48ea55f7e19a07fc013af91f065f1907
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-04-30 08:22:55 +0200

    check if it contains negative values, and extract

--- src/main/java/com/codurance/katalyst/StringCalculator.java
+++ src/main/java/com/codurance/katalyst/StringCalculator.java
@@ -1,7 +1,9 @@
 package com.codurance.katalyst;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class StringCalculator {
 
@@ -16,9 +18,13 @@ public class StringCalculator {
             separator = extractCustomSeparator(numbers);
             numbers = removeCustomSeparator(numbers);
         }
-        return Arrays.stream(numbers.split(separator, -1))
-            .map(number -> Integer.valueOf(number))
-            .reduce(0, (a, b) -> a + b);
+        List<Integer> values = Arrays.stream(numbers.split(separator, -1))
+                                .map(number -> Integer.valueOf(number))
+                                .collect(Collectors.toList());
+        if(containsNegativesNumbers(values)){
+            throw new NegativesNotAllowedException(extractNegativesNumbers(values));
+        }
+        return values.stream().reduce(0, (a, b) -> a + b);
     }
 
     private boolean containsCustomSeparator(String numbers){
@@ -33,4 +39,16 @@ public class StringCalculator {
         return numbers.substring(numbers.indexOf("\n")+1);
     }
     
+    private boolean containsNegativesNumbers(List<Integer> values) {
+        return values.stream()
+                .anyMatch(value -> value < 0);
+    }
+
+    private String[] extractNegativesNumbers(List<Integer> values) {
+        return values.stream()
+                .filter(value -> value < 0)
+                .map(value -> value.toString())
+                .toArray(String[]::new);
+    }
+
 }
