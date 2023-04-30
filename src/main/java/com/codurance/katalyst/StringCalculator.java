d074872490ab2fd1bab08411f6ebf226a62fa807
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-04-30 05:16:29 +0200

    find, extract and remove custom separator

--- src/main/java/com/codurance/katalyst/StringCalculator.java
+++ src/main/java/com/codurance/katalyst/StringCalculator.java
@@ -1,18 +1,36 @@
 package com.codurance.katalyst;
 
 import java.util.Arrays;
+import java.util.regex.Pattern;
 
 public class StringCalculator {
 
     private static final String DEFAULT_SEPARATOR = "[\\n|,]";
-    
+
     public Integer add(String numbers) {
         if("".equals(numbers)){
             return 0;
         }
-        return Arrays.stream(numbers.split(DEFAULT_SEPARATOR, -1))
+        String separator = DEFAULT_SEPARATOR;
+        if(containsCustomSeparator(numbers)){
+            separator = extractCustomSeparator(numbers);
+            numbers = removeCustomSeparator(numbers);
+        }
+        return Arrays.stream(numbers.split(separator, -1))
             .map(number -> Integer.valueOf(number))
             .reduce(0, (a, b) -> a + b);
+    }
+
+    private boolean containsCustomSeparator(String numbers){
+        return Pattern.compile("^[(\\/\\/.\\n)]").matcher(numbers).find();
+    }
+
+    private String extractCustomSeparator(String numbers){
+        return numbers.substring(numbers.indexOf("//")+2, numbers.indexOf("\n"));
+    }
+
+    private String removeCustomSeparator(String numbers){
+        return numbers.substring(numbers.indexOf("\n")+1);
     }
     
 }
--- src/test/java/com/codurance/katalyst/StringCalculatorTest.java
+++ src/test/java/com/codurance/katalyst/StringCalculatorTest.java
@@ -1,6 +1,9 @@
 package com.codurance.katalyst;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.regex.Pattern;
 
 import org.junit.jupiter.api.Test;
 
