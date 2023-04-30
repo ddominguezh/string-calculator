769661f506edee39d1558b091cf83722e3755f78
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-04-30 04:43:57 +0200

    refactor: extract static variable when default separator

--- src/main/java/com/codurance/katalyst/StringCalculator.java
+++ src/main/java/com/codurance/katalyst/StringCalculator.java
@@ -4,11 +4,13 @@ import java.util.Arrays;
 
 public class StringCalculator {
 
+    private static final String DEFAULT_SEPARATOR = "[\\n|,]";
+    
     public Integer add(String numbers) {
         if("".equals(numbers)){
             return 0;
         }
-        return Arrays.stream(numbers.split("[\\n|,]", -1))
+        return Arrays.stream(numbers.split(DEFAULT_SEPARATOR, -1))
             .map(number -> Integer.valueOf(number))
             .reduce(0, (a, b) -> a + b);
     }
